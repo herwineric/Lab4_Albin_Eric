1eb2d2c0eeba2e54dc6f4c4e2ef071cd80396a8a
Albin Vasterlund <abbe--93@hotmail.com>
2017-09-14 16:08:02 +0200

    hej

--- Albin.R
+++ Albin.R
@@ -1,2 +1,143 @@
+linreg<-function(formula,data){
+  
+  
+  x<-model.matrix(formula,data)
+  y<-all.vars(formula)[1]
+  y<-as.matrix(data[,names(data)==y])
 
-Hej<-10
+
+  b_hat<-solve(t(x)%*%x)%*%t(x)%*%y
+  
+  y_fits<-x%*%b_hat
+  
+  e<-y-y_fits
+  
+  df<-length(y)-(ncol(x)-1)
+  
+  var_e<-(t(e)%*%e)/df
+  
+  
+ var_b_hat<-as.numeric(var_e)*diag(solve((t(x)%*%x)))
+ 
+ t_b_hat<-as.numeric(b_hat)/sqrt(var_b_hat)
+  
+ p_b_hat<-(1-(pt(abs(t_b_hat),df = df)))*2
+ 
+ 
+ b_hat_numeric<-as.numeric(b_hat)
+ names(b_hat_numeric)<-rownames(b_hat)
+ 
+ result <- list(Call = formula, Coefficients = b_hat_numeric, Xterms=x, Yterms=y, Fitts=y_fits,
+                  Resid=e, df=df,
+                  Var_residuals=var_e, Var_betas=var_b_hat,
+                  tBetas=t_b_hat, Pvalues = p_b_hat)
+ 
+ class(result)<-"linreg"
+ 
+ 
+ print.linreg <<- function(obj) {
+   
+   beta<-as.character(round(obj$Coefficients,2))
+   namn<-names(obj$Coefficients)
+   
+   n<-max(c(nchar(beta),20))
+   if(TRUE%in%(nchar(namn)>n)){
+     namn[nchar(namn)>n]<-
+       paste(substr(namn[nchar(namn)>n],1,(n-3)),"...",sep="")
+   }
+   
+   for(i in 1:length(obj$Coefficients)){
+     if((nchar(namn)[i]==nchar(beta)[i])==FALSE){
+       
+       andra<-c(namn[i],beta[i])
+       ny<-andra
+       antal<-max(nchar(andra))
+       
+       lagga_till<-antal-min(nchar(andra))
+       plus<-paste(rep(" ",lagga_till),collapse = "")
+       ny[nchar(andra)<antal]<-paste(plus,andra[nchar(andra)<antal],sep = "")
+       
+       namn[i]<-ny[1]
+       beta[i]<-ny[2]
+     }
+   }
+   
+   
+   cat("Call:",sep="\n")
+   cat(paste(as.character(obj$Call)[2],as.character(obj$Call)[1],as.character(obj$Call)[3]), sep="\n")
+   cat(sep="\n")
+   cat("Coefficients:",sep="\n")
+   cat(namn,sep="\t")
+   cat("","\n")
+   cat(beta,sep="\t")
+ }
+ 
+ 
+ 
+ ###########
+ # reglin <- setRefClass("linreg",fields = c("Call", "Coefficients", "Xterms", "Yterms", "Fitts", 
+ #                                           "Resid", "df", "Var_residuals", 
+ #                                           "Var_betas", "tBetas", "Pvalues") )
+ # 
+ # result <- reglin(Call = formula, Coefficients = b_hat, Xterms=x, Yterms=y, Fitts=y_fits, 
+ #                  Resid=e, df=df, 
+ #                  Var_residuals=var_e, Var_betas=var_b_hat, 
+ #                  tBetas=t_b_hat, Pvalues = p_b_hat)
+ #########
+ 
+ return(result)
+
+}
+####### 
+print.linreg <- function(obj) {
+  
+  beta<-as.character(round(obj$Coefficients,2))
+  namn<-names(obj$Coefficients)
+  
+  n<-max(c(nchar(beta),20))
+  if(TRUE%in%(nchar(namn)>n)){
+    namn[nchar(namn)>n]<-
+      paste(substr(namn[nchar(namn)>n],1,(n-3)),"...",sep="")
+  }
+  
+  for(i in 1:length(obj$Coefficients)){
+    if((nchar(namn)[i]==nchar(beta)[i])==FALSE){
+      
+      andra<-c(namn[i],beta[i])
+      ny<-andra
+      antal<-max(nchar(andra))
+      
+      lagga_till<-antal-min(nchar(andra))
+      plus<-paste(rep(" ",lagga_till),collapse = "")
+      ny[nchar(andra)<antal]<-paste(plus,andra[nchar(andra)<antal],sep = "")
+      
+      namn[i]<-ny[1]
+      beta[i]<-ny[2]
+    }
+  }
+  
+  
+  cat("Call:",sep="\n")
+  cat(paste(as.character(obj$Call)[2],as.character(obj$Call)[1],as.character(obj$Call)[3]), sep="\n")
+  cat(sep="\n")
+  cat("Coefficients:",sep="\n")
+  cat(namn,sep="\t")
+  cat("","\n")
+  cat(beta,sep="\t")
+}
+##############
+data(iris)
+lm(Petal.Length~Species, data = iris)
+linreg(Petal.Length~Species, data = iris)
+
+
+
+
+
+
+
+
+
+
+
+
