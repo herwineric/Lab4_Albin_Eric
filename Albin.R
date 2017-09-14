942c0239e17069a4fddb507f3b20964452e0036b
Albin Vasterlund <abbe--93@hotmail.com>
2017-09-14 17:35:19 +0200

    ehj

--- Albin.R
+++ Albin.R
@@ -1,3 +1,4 @@
+####  linreg ####
 linreg<-function(formula,data){
   
   
@@ -88,53 +89,110 @@ linreg<-function(formula,data){
  return(result)
 
 }
-####### 
-print.linreg <- function(obj) {
+##############
+data(iris)
+summary(lm(Petal.Length~Species, data = iris))
+obj<-linreg(Petal.Length~Species, data = iris)
+obj<-linreg(formula, data = data)
+
+obj
+
+##############
+summary.linreg <- function(obj){
   
-  beta<-as.character(round(obj$Coefficients,2))
   namn<-names(obj$Coefficients)
   
-  n<-max(c(nchar(beta),20))
+  n<-max(c(nchar(namn),20))
   if(TRUE%in%(nchar(namn)>n)){
     namn[nchar(namn)>n]<-
       paste(substr(namn[nchar(namn)>n],1,(n-3)),"...",sep="")
   }
+  n<-max(nchar(namn))
+
   
-  for(i in 1:length(obj$Coefficients)){
-    if((nchar(namn)[i]==nchar(beta)[i])==FALSE){
+  for(i in 1:length(namn)){
+    if(nchar(namn[i])<n){
       
-      andra<-c(namn[i],beta[i])
-      ny<-andra
-      antal<-max(nchar(andra))
-      
-      lagga_till<-antal-min(nchar(andra))
+      lagga_till<-n-nchar(namn[i])
       plus<-paste(rep(" ",lagga_till),collapse = "")
-      ny[nchar(andra)<antal]<-paste(plus,andra[nchar(andra)<antal],sep = "")
-      
-      namn[i]<-ny[1]
-      beta[i]<-ny[2]
+      namn[i]<-paste(plus,namn[i],sep = "")
     }
   }
+  plus<-paste(rep(" ",n),collapse = "")
+  namn<-c(plus,namn)
   
   
-  cat("Call:",sep="\n")
-  cat(paste(as.character(obj$Call)[2],as.character(obj$Call)[1],as.character(obj$Call)[3]), sep="\n")
-  cat(sep="\n")
+
+  Estimate<-as.character(round(obj$Coefficients,3))
+  n<-max(nchar(Estimate),nchar("Estimate"))
+  for(i in 1:length(Estimate)){
+    if(nchar(Estimate[i])<n){
+      lagga_till<-n-nchar(Estimate[i])
+      plus<-paste(rep(" ",lagga_till),collapse = "")
+      Estimate[i]<-paste(plus,Estimate[i],sep = "")
+    }
+  }
+  Estimate<-c("Estimate",Estimate)
+  
+  
+  Std_Error<-as.character(round(obj$Var_betas,3))
+  n<-max(nchar(c(Std_Error,"Std.Error")))
+  for(i in 1:length(Std_Error)){
+    if(nchar(Std_Error[i])<n){
+      lagga_till<-n-nchar(Std_Error[i])
+      plus<-paste(rep(" ",lagga_till),collapse = "")
+      Std_Error[i]<-paste(plus,Std_Error[i],sep = "")
+    }
+  }
+  Std_Error<-c("Std.Error",Std_Error)
+  
+  
+  t_value<-as.character(round(obj$tBetas,3))
+  n<-max(nchar(c(t_value,"t value")))
+  for(i in 1:length(t_value)){
+    if(nchar(t_value[i])<n){
+      lagga_till<-n-nchar(t_value[i])
+      plus<-paste(rep(" ",lagga_till),collapse = "")
+      t_value[i]<-paste(plus,t_value[i],sep = "")
+    }
+  }
+  t_value<-c("t value",t_value)
+  
+  
+  Pr_t<-as.character(round(obj$Pvalues,4))
+  n<-max(nchar(c(Pr_t,"Pr(>|t|)")))
+  Pr_t[Pr_t=="0"]<-"<0.0001"
+  for(i in 1:length(Pr_t)){
+    if(nchar(Pr_t[i])<n){
+      lagga_till<-n-nchar(Pr_t[i])
+      plus<-paste(rep(" ",lagga_till),collapse = "")
+      Pr_t[i]<-paste(plus,Pr_t[i],sep = "")
+    }
+  }
+  Pr_t<-c("Pr(>|t|)",Pr_t)
+  
+
+  
+  
+  
+  
+  
+  cat("Call:", "\n")
+  cat(paste(as.character(obj$Call)[2],as.character(obj$Call)[1],as.character(obj$Call)[3]), sep="\n") 
+  cat(" ",sep="\n")
   cat("Coefficients:",sep="\n")
-  cat(namn,sep="\t")
-  cat("","\n")
-  cat(beta,sep="\t")
+  cat(paste(namn,Estimate,Std_Error,t_value,Pr_t),sep="\n")
+  cat(" ",sep="\n")
+  cat( paste("Residual standard error (\u03C3):",round(obj$Var_residuals,5)),sep="\n")
+  cat( paste("df:",obj$df),sep="\n")
+  
+  
+
 }
-##############
-data(iris)
-lm(Petal.Length~Species, data = iris)
-linreg(Petal.Length~Species, data = iris)
 
+summary.linreg(obj)
 
-
-
-
-
+#
 
 
 
